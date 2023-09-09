7f1ef08ca697f1682e1cc8e5844d9f25aa4960d3
James Schinwald <james@payspace.ca>
2023-09-09 13:18:56 -0400

    feat: added config editor

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -9,11 +9,46 @@ alias star_wars='nc towel.blinkenlights.nl 23'
 # +-----------------+
 # | GENERAL ALIASES |
 # ===================
+edit_config () {
+	declare -A config_list
+
+	config_list[aliases]="$HOME/.zsh/aliases.zsh"
+	config_list[i3]="$HOME/.config/i3/config"
+	config_list[picom]="$HOME/.config/picom/picom.conf"
+	config_list[polybar]="$HOME/.config/polybar/config.ini"
+	config_list[sxhkd]="$HOME/.config/sxhkd/launch.sh"
+	config_list[rofi]="$HOME/.config/rofi/config.rasi"
+	config_list[redshift]="$HOME/.config/redshift/redshift.conf"
+	config_list[kitty]="$HOME/.config/kitty/kitty.conf"
+	config_list[nvim]="$HOME/.config/nvim/init.lua"
+	config_list[starship]="$HOME/.config/starship.toml"
+	config_list[flameshot]="$HOME/.config/flameshot/flameshot.ini"
+	config_list[ssh]="$HOME/.ssh/config"
+
+	config_keys=`for key in "${(@k)config_list}"; do; echo $key; done`
+
+	if [[ -z "$1" ]]; then
+		config_chosen=`echo $config_keys | fzf`
+	elif [[ -z "${config_list[$1]}" ]]; then
+		config_chosen=`echo $config_keys | fzf -q $1 -1`
+	else
+		config_chosen="$1"
+	fi
+
+	if [[ -z "${config_list[$config_chosen]}" ]]; then
+		return 0
+	fi
+	
+	v ${config_list[$config_chosen]}
+}
+
 port_used () {
   sudo netstat -anpe | grep -P "[[:space:]]$1[[:space:]]" | grep "LISTEN"
   # sudo lsof -i :$1
 }
 
+alias cfgs=edit_config
+alias src='source ~/.zshrc'
 alias c='clear'
 alias l='ll'
 alias ls='exa --header --group-directories-first -t=mod --time-style=long-iso'
