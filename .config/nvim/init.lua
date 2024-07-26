ccfefa4923de24c466c1efb57028b36faaed8816
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-07-26 09:45:49 -0400

    feat: added nvim plugins and configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,6 +27,3 @@ require("config.keymaps")
 require("config.options")
 require("config.autocmds")
 require("config.extras")
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,22 @@
+return {
+	"stevearc/oil.nvim",
+	keys = {
+		{
+			"-",
+			mode = { "n" },
+			function()
+				require("oil").open()
+			end,
+			{ desc = "Open parent directory" },
+		},
+	},
+	opts = {
+		default_file_explorer = true,
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
