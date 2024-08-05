935631ce42868e51746d124ee15799aa259f42a8
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-08-05 17:01:33 -0400

    fix: added fzf keybinds

--- .zsh/plugins/10-fzf.zsh
+++ .zsh/plugins/10-fzf.zsh
@@ -1,17 +1,6 @@
 # Setup fzf
-# ---------
-if [[ ! "$PATH" == */home/james/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
-fi
+source <(fzf --zsh)
 
-# Settings
-# --------
+# Options
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && zsh-defer source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-# source "/home/james/.fzf/shell/key-bindings.zsh"
