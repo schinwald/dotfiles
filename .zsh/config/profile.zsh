e5f71f757653ad8605d2d2372bae055471eaeb31
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-08-06 20:28:03 -0400

    feat: added dotfiles environment

--- .zsh/config/profile.zsh
+++ .zsh/config/profile.zsh
@@ -1,12 +1,14 @@
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 export MANPATH="/usr/local/man:$MANPATH"
 export LANG="en_US.UTF-8"
+export DOTFILES="$HOME/.dotfiles"
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
   export EDITOR='vim'
 else
-  export EDITOR='mvim'
+  # Alias for nvim
+  export EDITOR='vim'
 fi
 
 export VISUAL="$EDITOR"
