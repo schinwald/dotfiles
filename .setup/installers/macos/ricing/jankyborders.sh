81649a8fe2e756ea2219fef438ac78525687230b
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-08-11 16:50:55 -0400

    feat: added jankyborders installer

---
+++ .setup/installers/macos/ricing/jankyborders.sh
@@ -0,0 +1,4 @@
+# Install jankyborders (https://github.com/FelixKratz/JankyBorders)
+prompt "Would you like to install jankyborders (adds borders to windows)?"
+brew tap FelixKratz/formulae
+brew install borders
