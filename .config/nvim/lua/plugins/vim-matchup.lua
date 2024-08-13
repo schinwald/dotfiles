0cb19f02ad8de0d7ef8d5e0b8f7303e392d0e94c
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-08-13 09:06:41 -0400

    fix: removed vim-matchup from vscode as it was causing issues

--- .config/nvim/lua/plugins/vim-matchup.lua
+++ .config/nvim/lua/plugins/vim-matchup.lua
@@ -1,6 +1,13 @@
 -- TODO: configure this properly
 return {
 	"andymass/vim-matchup",
+	enabled = function()
+		if vim.g.vscode then
+			return false
+		end
+
+		return true
+	end,
 	setup = function()
 		vim.g.matchup_matchparen_offscreen = { method = "popup" }
 	end,
