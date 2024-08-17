7aa90d6ca2310e16a120d472c08a69ae0c6430bd
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-08-16 23:03:25 -0400

    feat: added keys to neogit

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -7,6 +7,41 @@ return {
 
 		return true
 	end,
+	lazy = false,
+	keys = {
+		{
+			"<leader>gs",
+			function()
+				require("neogit").open()
+			end,
+			{ desc = "[G]it [S]tatus" },
+		},
+		{
+			"<leader>gc",
+			":Neogit commit<CR>",
+			{ desc = "[G]it [C]ommit" },
+		},
+		{
+			"<leader>gp",
+			":Neogit pull<CR>",
+			{ desc = "[G]it [P]ull" },
+		},
+		{
+			"<leader>gP",
+			":Neogit push<CR>",
+			{ desc = "[G]it [P]ush" },
+		},
+		{
+			"<leader>gb",
+			":Telescope git_branches<CR>",
+			{ desc = "Switch [G]it [B]ranch" },
+		},
+		{
+			"<leader>gB",
+			":G blame<CR>",
+			{ desc = "Toggle [G]it [B]lame" },
+		},
+	},
 	config = true,
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
