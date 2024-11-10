8394259f15856fc744eb1f6e0a3a72d009a6edd0
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-11-09 21:49:18 -0500

    feat: added eldritch theme to nvim

--- .config/nvim/lua/plugins/eldritch-nvim.lua
+++ .config/nvim/lua/plugins/eldritch-nvim.lua
@@ -1,5 +1,12 @@
 return {
 	"eldritch-theme/eldritch.nvim",
+	enabled = function()
+		if vim.g.vscode then
+			return false
+		end
+
+		return true
+	end,
 	lazy = false,
 	priority = 1000,
 	opts = {},
