0cfab8a0098ab6e532d6e5888543dcb89241d551
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-11-09 22:09:57 -0500

    fix: added vscode disabled for nvim autopairs

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,6 +1,13 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
+	enabled = function()
+		if vim.g.vscode then
+			return false
+		end
+
+		return true
+	end,
 	config = true,
 	-- use opts = {} for passing setup options
 	-- this is equivalent to setup({}) function
