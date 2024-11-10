69420ddc4acf24f76d796c02dfa3089eea4063de
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-11-10 16:35:15 -0500

    fix: fixed git directory inference

--- .config/nvim/lua/config/extras.lua
+++ .config/nvim/lua/config/extras.lua
@@ -1,16 +1,19 @@
 -- Gets the current folder
 vim.fn.where_am_i = function()
-	local path = vim.fn.expand("%")
-	return vim.system({ "dirname", path }, { text = true }):wait().stdout
+	local path = vim.fn.expand("%:p")
+	return string.sub(vim.system({ "dirname", path }, { text = true }):wait().stdout, 1, -2)
 end
 
 -- Sets the environment variable for the default git
 vim.fn.use_default_git = function()
-	local code = vim.system({ "git", "status" }):wait().code
-
 	-- If no git repository is found then default to the dotfile one
-	if code ~= 0 then
-		vim.env.GIT_DIR = vim.env.DOTFILES
-		vim.env.GIT_WORK_TREE = vim.env.HOME
+	local where_am_i = vim.fn.where_am_i()
+	if vim.loop.fs_stat(where_am_i) then
+		local code = vim.system({ "git", "status" }, { cwd = where_am_i }):wait().code
+		if code ~= 0 then
+			vim.env.GIT_DIR = vim.env.DOTFILES
+			vim.env.GIT_WORK_TREE = vim.env.HOME
+			print("git")
+		end
 	end
 end
