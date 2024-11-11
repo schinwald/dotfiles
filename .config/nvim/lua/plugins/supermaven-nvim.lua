868415616fabf9542d8898b001cf59d514d8885a
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-11-10 20:27:33 -0500

    feat: updated supermaven keymaps

--- .config/nvim/lua/plugins/supermaven-nvim.lua
+++ .config/nvim/lua/plugins/supermaven-nvim.lua
@@ -8,6 +8,23 @@ return {
 		return true
 	end,
 	config = function()
-		require("supermaven-nvim").setup({})
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<C-S-y>",
+			},
+			ignore_filetypes = { cpp = true },
+			color = {
+				-- TODO: update colors to be more subtle
+				-- suggestion_color = "#ffffff",
+				-- cterm = 244,
+			},
+			log_level = "off",
+			disable_inline_completion = false,
+			disable_keymaps = false,
+			-- Condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+			condition = function()
+				return false
+			end,
+		})
 	end,
 }
