c29f1350f091d31acebecdf5ec5bc9c427d9a622
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-11-10 19:01:41 -0500

    feat: removed deprecated loop in favor of uv

--- .config/nvim/lua/config/extras.lua
+++ .config/nvim/lua/config/extras.lua
@@ -8,7 +8,7 @@ end
 vim.fn.use_default_git = function()
 	-- If no git repository is found then default to the dotfile one
 	local where_am_i = vim.fn.where_am_i()
-	if vim.loop.fs_stat(where_am_i) then
+	if vim.uv.fs_stat(where_am_i) then
 		local code = vim.system({ "git", "status" }, { cwd = where_am_i }):wait().code
 		if code ~= 0 then
 			vim.env.GIT_DIR = vim.env.DOTFILES
