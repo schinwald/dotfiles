3a21d6fa39e60bed87bba1f8802358033833de42
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-11-23 22:47:39 -0500

    feat: added nvim-highlight-colors

---
+++ .config/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,11 @@
+-- https://github.com/brenoprata10/nvim-highlight-colors
+-- [[ Configure nvim-highlight-colors ]] See `:help nvim-highlight-colors`
+
+return {
+	"brenoprata10/nvim-highlight-colors",
+	opts = {
+		render = "virtual",
+		enable_tailwind = true,
+	},
+	config = true,
+}
