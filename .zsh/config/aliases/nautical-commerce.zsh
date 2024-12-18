76b8d0427b4cbb9cef29d14c2c2ecdd0ae77b31d
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-12-17 22:54:08 -0500

    feat: added more work aliases

--- .zsh/config/aliases/nautical-commerce.zsh
+++ .zsh/config/aliases/nautical-commerce.zsh
@@ -5,8 +5,10 @@
 # ==============================
 # nautical-api
 NAUTICAL_API_PATH="~/Projects/nautical-api"
-alias api-exec='$NAUTICAL_API_PATH/nautical-execute'
+alias api-exec='$NAUTICAL_API_PATH/nauticalexecute'
 alias api-db-main='$HOME/.config/gcloud/cloud-sql-proxy -p 5433 nautical-commerce:us-central1:nautical-dev-shared-us'
 
 # storefront, dashboard, checkout, etc
-alias ys='yarn install && yarn generate && yarn dev'
+alias ys='yarn install && yarn generate'
+alias yc='ys && yarn typecheck'
+alias yd='ys && yarn dev'
