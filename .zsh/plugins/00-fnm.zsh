5f0c34a378cda0121d444c4415ccf0dde3286e49
James Schinwald <james.schinwald@nauticalcommerce.com>
2024-12-26 23:34:27 -0500

    feat: added silent logging for fnm

--- .zsh/plugins/00-fnm.zsh
+++ .zsh/plugins/00-fnm.zsh
@@ -1,6 +1,6 @@
 FNM_PATH="$HOME/.local/share/fnm"
 if [ -d "$FNM_PATH" ]; then
   export PATH="$HOME/.local/share/fnm:$PATH"
-  eval "$(fnm env --use-on-cd --version-file-strategy=recursive --resolve-engines --shell zsh)"
+  eval "$(fnm env --use-on-cd --version-file-strategy=recursive --log-level=quiet --resolve-engines --shell zsh)"
 fi
 
