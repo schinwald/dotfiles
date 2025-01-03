e7124e03ac6bec9c4b46ee4152b6ecf91c70c583
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-01-02 21:45:07 -0500

    fix: added treesitter highlighting

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -30,13 +30,11 @@ return {
 		-- Autoinstall languages that are not installed
 		auto_install = true,
 		highlight = {
-			enabled = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
+			enable = true,
 		},
-		indent = { enabled = true, disable = { "ruby" } },
+		indent = {
+			enable = true,
+		},
 	},
 	---@param opts TSConfig
 	config = function(_, opts)
