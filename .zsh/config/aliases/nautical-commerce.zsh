d99015fe700bd9f327ca76b875deb86379092bf5
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-01-17 10:33:45 -0500

    feat: added aliases to make developing at work easier

--- .zsh/config/aliases/nautical-commerce.zsh
+++ .zsh/config/aliases/nautical-commerce.zsh
@@ -8,7 +8,13 @@ NAUTICAL_API_PATH="~/Projects/nautical-api"
 alias api-exec='$NAUTICAL_API_PATH/nauticalexecute'
 alias api-db-main='$HOME/.config/gcloud/cloud-sql-proxy -p 5433 nautical-commerce:us-central1:nautical-dev-shared-us'
 
+# ingestion
+export yii='PGPASSWORD=nautical && createdb -h 127.0.0.1 -p 5432 -U nautical ingestion -w'
+export yis='yarn db:migrate'
+export yid='yis && yarn dev'
+
 # storefront, dashboard, checkout, etc
 alias ys='yarn install && yarn generate'
 alias yc='ys && yarn typecheck'
 alias yd='ys && yarn dev'
+alias yus='yarn add @nautical-commerce/graphql-schema@next'
