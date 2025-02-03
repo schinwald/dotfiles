6df43427bf432136a601e78d690ec5ab6273b8ae
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-02-03 07:21:45 -0500

    fix(alias): fixed cloud-sql-proxy wrong path to executable

--- .zsh/config/aliases/nautical-commerce.zsh
+++ .zsh/config/aliases/nautical-commerce.zsh
@@ -6,7 +6,7 @@
 # nautical-api
 NAUTICAL_API_PATH="~/Projects/nautical-api"
 alias api-exec='$NAUTICAL_API_PATH/nauticalexecute'
-alias api-db-main='$HOME/.config/gcloud/cloud-sql-proxy --run-connection-test -p 5433 nautical-commerce:us-central1:nautical-dev-shared-us'
+alias api-db-main='cloud-sql-proxy --run-connection-test -p 5433 nautical-commerce:us-central1:nautical-dev-shared-us'
 
 # ingestion
 export yii='PGPASSWORD=nautical && createdb -h 127.0.0.1 -p 5432 -U nautical ingestion -w'
