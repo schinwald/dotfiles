5ffdb7028a2450367fd93bfdb02add8b42a1f09e
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-02-06 21:41:59 -0500

    feat(neovim): added tmux navigation plugin

---
+++ .config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -0,0 +1,6 @@
+-- https://github.com/alexghergh/nvim-tmux-navigation?tab=readme-ov-file
+-- [[ Configure Nvim Tmux Navigation ]] See `:help nvim-tmux-navigation`
+
+return {
+	"alexghergh/nvim-tmux-navigation",
+}
