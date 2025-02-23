f0fdffd7bd7a353c8efa03796424272d5a6512e5
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-02-23 12:02:23 -0500

    fix: updated autocomplete key for ghostty and tmux support

--- .zsh/plugins/40-zsh-autosuggestions.zsh
+++ .zsh/plugins/40-zsh-autosuggestions.zsh
@@ -1,2 +1,9 @@
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# Accepts the auto suggestion inside of ghostty
+bindkey '^[[121;6u' end-of-line
+
+# Accepts the auto suggestion inside of tmux
+# I have to do this because tmux extended-keys only affect applications inside
+# of tmux, not the terminal itself, so this is C-S-y in tmux
 bindkey '^Y' end-of-line
