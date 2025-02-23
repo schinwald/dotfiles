7ff00491951a283d47c2d93ba9d75566325494aa
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-02-23 14:13:49 -0500

    feat(nvim): updated nvim-tmux-navigation with keybinds

--- .config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ .config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -1,6 +1,22 @@
--- https://github.com/alexghergh/nvim-tmux-navigation?tab=readme-ov-file
+-- https://github.com/alexghergh/nvim-tmux-navigation
 -- [[ Configure Nvim Tmux Navigation ]] See `:help nvim-tmux-navigation`
 
 return {
 	"alexghergh/nvim-tmux-navigation",
+	enabled = function()
+		---@diagnostic disable: undefined-field
+		if vim.g.vscode then
+			return false
+		end
+
+		return true
+	end,
+	config = function()
+		local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+		vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+		vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+		vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+		vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+	end,
 }
