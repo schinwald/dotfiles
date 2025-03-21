e839b768d879f248dc82facb75328b5669bb1673
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-03-20 23:02:33 -0400

    feat(nvim): add move open and close for easier file movement

--- .config/nvim/lua/config/extras.lua
+++ .config/nvim/lua/config/extras.lua
@@ -30,3 +30,29 @@ vim.fn.get_parent_node_by_type = function(node, type)
 	print(current_node)
 	return current_node
 end
+
+---Creates two windows to easily move files between directories
+vim.api.nvim_create_user_command("MoveOpen", function()
+	local oil = require("oil")
+
+	vim.cmd("tab split")
+
+	-- Open source and destination directories
+	local left_window = vim.api.nvim_get_current_win()
+	vim.cmd("vsplit")
+	local right_window = vim.api.nvim_get_current_win()
+
+	vim.api.nvim_set_current_win(left_window)
+	oil.open("~/Downloads")
+
+	vim.api.nvim_set_current_win(right_window)
+	oil.open()
+	oil.set_sort({ { "atime", "desc" } })
+
+	vim.api.nvim_set_current_win(left_window)
+end, {})
+
+---Creates two windows to easily move files between directories
+vim.api.nvim_create_user_command("MoveClose", function()
+	vim.cmd("tabclose")
+end, {})
