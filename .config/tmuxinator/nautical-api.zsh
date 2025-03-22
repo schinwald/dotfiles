37141599bc0c85eed7c46b34adada9c8faf153cf
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-03-22 14:22:26 -0400

    feat(tmuxinator): update nautical-api zsh

--- .config/tmuxinator/nautical-api.zsh
+++ .config/tmuxinator/nautical-api.zsh
@@ -1,5 +1,10 @@
 #! /bin/zsh
 
 NAUTICAL_API_PATH="~/Projects/nautical-api"
-alias api-exec='$NAUTICAL_API_PATH/nauticalexecute'
-alias api-db-main='cloud-sql-proxy --run-connection-test -p 5433 nautical-commerce:us-central1:nautical-dev-shared-us'
+alias nauticalexecute='$NAUTICAL_API_PATH/nauticalexecute'
+alias gcloudmain='cloud-sql-proxy --run-connection-test -p 5433 nautical-commerce:us-central1:nautical-dev-shared-us'
+alias tests=runtests
+
+runtests () {
+	docker-compose run --rm api poetry run pytest -vv -n auto -m "not integration" --reuse-db $@
+}
