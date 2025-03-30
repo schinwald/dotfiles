b152de9e43d2a5c9559a953bf60858b647f30a0e
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-03-30 01:22:38 -0400

    feat(nvim): add nicer input prompt for zoxide

--- .config/nvim/lua/config/extras.lua
+++ .config/nvim/lua/config/extras.lua
@@ -59,8 +59,13 @@ end, {})
 
 ---Creates a command to quickly open a file in the current directory
 vim.api.nvim_create_user_command("Zoxide", function()
-	vim.ui.input({ prompt = "z " }, function(input)
-		local result = vim.system({ "zoxide", "query", input }, { text = true }):wait()
+	local input = require("snacks").input.input
+	input({
+		icon = "📁",
+		prompt = "Go to directory",
+		prompt_pos = "title",
+	}, function(query)
+		local result = vim.system({ "zoxide", "query", query }, { text = true }):wait()
 		if result.code ~= 0 then
 			vim.notify(result.stderr, vim.log.levels.ERROR)
 			return
