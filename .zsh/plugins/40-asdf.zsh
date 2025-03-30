71584592b24706e23a76e16a2cff1a6db21cb95e
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-03-30 01:23:48 -0400

    fix(zshrc): removed asdf duplicate re-init

--- .zsh/plugins/40-asdf.zsh
+++ .zsh/plugins/40-asdf.zsh
@@ -1,11 +1,7 @@
 export ASDF_DATA_DIR="$HOME/.asdf"
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
-# Autocompletions
-mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
-asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
-
-# Append completions to fpath
+# Add autocompletions to fpath
 fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
 
 # Initialise completions
