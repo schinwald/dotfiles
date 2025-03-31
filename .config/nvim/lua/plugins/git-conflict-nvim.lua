0ba8a5e4d4fafa4158981fbfd919533781aff1cc
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-03-30 21:48:57 -0400

    feat(nvim): add git conflict plugin

---
+++ .config/nvim/lua/plugins/git-conflict-nvim.lua
@@ -0,0 +1,8 @@
+-- https://github.com/akinsho/git-conflict.nvim
+-- [[ Configure Git Conflict ]] See `:help git-conflict.txt`
+
+return {
+	"akinsho/git-conflict.nvim",
+	version = "*",
+	config = true,
+}
