372616c7ae85211f9eccdfcaef574de58e1c3c30
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-06-14 23:31:38 -0400

    fix(zsh): reset zle widget before evaluating starship

--- .zsh/plugins/10-starship.zsh
+++ .zsh/plugins/10-starship.zsh
@@ -1,3 +1,10 @@
 # Setup starship
+
+# Makes sure the zle widget is reset before evaluating starship
+if [[ "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select" || \
+      "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select-wrapped" ]]; then
+  zle -N zle-keymap-select "";
+fi
+
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 eval "$(starship init zsh)"
