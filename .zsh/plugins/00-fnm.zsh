b8b6ee789375083b68450e74c6da986914d0e86a
James Schinwald <james.schinwald@nauticalcommerce.com>
2025-07-15 19:22:06 -0400

    feat(zsh): delete fnm in favor of mise

--- .zsh/plugins/00-fnm.zsh
+++
@@ -1,6 +0,0 @@
-FNM_PATH="$HOME/.local/share/fnm"
-if [ -d "$FNM_PATH" ]; then
-  export PATH="$HOME/.local/share/fnm:$PATH"
-  zsh-defer eval "$(fnm env --use-on-cd --version-file-strategy=recursive --log-level=quiet --resolve-engines --shell zsh)"
-fi
-
