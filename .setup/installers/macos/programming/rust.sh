77bf44aa5af3230235ae88831f7d21658fb999c5
James Schinwald <james@spruce.local>
2025-08-16 10:58:48 -0400

    feat: delete all programming installers

--- .setup/installers/macos/programming/rust.sh
+++
@@ -1,3 +0,0 @@
-# Install rust
-prompt "Would you like to install rust?"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
--- .setup/installers/macos/programming/zsh.sh
+++
@@ -1,3 +0,0 @@
-# Install zsh
-prompt "Would you like to install zsh?"
-brew install zsh
