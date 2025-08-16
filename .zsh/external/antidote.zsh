dcc49e762d0ed3f4ca98b33da958f04e3cb36b2e
James Schinwald <james@spruce.local>
2025-08-16 11:00:55 -0400

    feat: delete antidote zsh file

--- .zsh/external/antidote.zsh
+++
@@ -1,2 +0,0 @@
-fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
-source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
