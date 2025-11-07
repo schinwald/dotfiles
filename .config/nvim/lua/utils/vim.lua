bb514fb8de51cc181aaed957d2f9e3663be12e1a
schinwald <jamesschinwald@gmail.com>
2025-11-07 16:53:56 -0500

    fix(nvim): use_default_git uses rev-parse now and handle oil

--- .config/nvim/lua/utils/vim.lua
+++ .config/nvim/lua/utils/vim.lua
@@ -1,19 +1,25 @@
 ---Gets the current folder
 vim.fn.where_am_i = function()
-	local path = vim.fn.expand("%:p")
-	return string.sub(vim.system({ "dirname", path }, { text = true }):wait().stdout, 1, -2)
+	return vim.fn.fnamemodify(vim.fn.expand("%:p"), ":h")
 end
 
 ---Sets the environment variable for the default git
 vim.fn.use_default_git = function()
 	-- If no git repository is found then default to the dotfile one
 	local where_am_i = vim.fn.where_am_i()
-	if vim.uv.fs_stat(where_am_i) then
-		local code = vim.system({ "git", "status" }, { cwd = where_am_i }):wait().code
-		if code ~= 0 then
-			vim.env.GIT_DIR = vim.env.DOTFILES
-			vim.env.GIT_WORK_TREE = vim.env.HOME
-		end
+	if not where_am_i or where_am_i == "" then
+		return
+	end
+
+	local cleaned = where_am_i:gsub("^oil://", "")
+
+	-- Check if inside of a git repo
+	local result = vim.system({ "git", "rev-parse", "--is-inside-work-tree" }, { cwd = cleaned }):wait()
+
+	-- Fallback to dotfiles
+	if result.code ~= 0 then
+		vim.env.GIT_DIR = vim.env.DOTFILES
+		vim.env.GIT_WORK_TREE = vim.env.HOME
 	end
 end
 
@@ -24,10 +30,8 @@ end
 vim.fn.get_parent_node_by_type = function(node, type)
 	local current_node = node
 	while current_node ~= nil and current_node:type() ~= type do
-		print(current_node)
 		current_node = current_node:parent()
 	end
-	print(current_node)
 	return current_node
 end
 
