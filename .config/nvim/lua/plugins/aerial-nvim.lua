fb7992c7ab833828f3ba414654cfa5ccdd00244a
schinwald <jamesschinwald@gmail.com>
2025-11-07 16:51:15 -0500

    feat(nvim): disable another plugin for vscode

--- .config/nvim/lua/plugins/aerial-nvim.lua
+++ .config/nvim/lua/plugins/aerial-nvim.lua
@@ -3,6 +3,15 @@
 
 return {
 	"stevearc/aerial.nvim",
+	enabled = function()
+		---@diagnostic disable: undefined-field
+		if vim.g.vscode then
+			return false
+		end
+
+		return true
+	end,
+	lazy = false,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
