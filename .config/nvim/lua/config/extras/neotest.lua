2f0a970c127b411353005bc2c7c708752df49a9d
schinwald <jamesschinwald@gmail.com>
2025-12-06 21:11:33 -0500

    feat(nvim): improve neotest open

--- .config/nvim/lua/config/extras/neotest.lua
+++ .config/nvim/lua/config/extras/neotest.lua
@@ -6,6 +6,9 @@ local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
+local MRU = require("utils.mru")
+local json = require("utils.json")
+
 -- Remove wrap from Neotest output panel
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "neotest-output-panel",
@@ -34,30 +37,99 @@ local function pick_npm_script(opts)
 		return
 	end
 
-	local scripts = vim.g.npm.get_script_names()
-	if not scripts or vim.tbl_isempty(scripts) then
+	local package_directory = vim.g.npm.find_package_directory()
+	if not package_directory then
+		vim.notify("No package directory found", vim.log.levels.WARN)
+		return
+	end
+
+	local mru = MRU.new({
+		name = "neotest-open/" .. string.encode(package_directory, "percent"),
+		max_entries = 1000,
+	})
+
+	local all_scripts = table.map(vim.g.npm.get_script_names(), function(value)
+		return {
+			type = "package",
+			data = value,
+		}
+	end)
+
+	if not all_scripts or vim.tbl_isempty(all_scripts) then
 		vim.notify("No scripts found in package.json", vim.log.levels.WARN)
 		return
+	end
+
+	local recent_scripts = table.map(mru:get_recent(10), function(value)
+		return json.decode(value)
+	end)
+
+	local delimiter = {
+		{
+			type = "decoration",
+			data = "───────────────────────────────────────────────────────────",
+		},
+	}
+
+	local results = all_scripts
+	if #recent_scripts > 0 then
+		results = table.merge(recent_scripts, delimiter, all_scripts)
 	end
 
 	pickers
 		.new(opts, {
 			prompt_title = "Select script",
-			finder = finders.new_table({ results = scripts }),
-			sorter = conf.generic_sorter(opts),
+			finder = finders.new_table({
+				results = results,
+				entry_maker = function(entry)
+					local prefix
+					if entry.type == "package" then
+						prefix = package_manager .. ": "
+					elseif entry.type == "custom" then
+						prefix = "custom: "
+					else
+						prefix = ""
+					end
 
+					return {
+						value = entry,
+						display = prefix .. entry.data,
+						ordinal = entry.data,
+					}
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
 			attach_mappings = function(prompt_bufnr, map)
 				local function on_select()
 					local selected = action_state.get_selected_entry()
-					local script = selected[1]
 
-					local get_command_from_package_manager = {
-						npm = "npm run " .. script,
-						pnpm = "pnpm " .. script,
-						yarn = "yarn " .. script,
+					if selected == nil then
+						local input = action_state.get_current_line()
+						selected = {
+							value = {
+								type = "custom",
+								data = input,
+							},
+						}
+					end
+
+					mru:update(json.encode(selected.value, { sorted = true }))
+
+					local type
+					if selected.value.type == "package" then
+						type = package_manager
+					elseif selected.value.type == "custom" then
+						type = "custom"
+					end
+
+					local get_command_from_type = {
+						npm = "npm run " .. selected.value.data,
+						pnpm = "pnpm " .. selected.value.data,
+						yarn = "yarn " .. selected.value.data,
+						custom = selected.value.data,
 					}
 
-					vim.g.neotest_command = get_command_from_package_manager[package_manager] .. " --"
+					vim.g.neotest_command = get_command_from_type[type] .. " --"
 
 					actions.close(prompt_bufnr)
 
@@ -65,8 +137,12 @@ local function pick_npm_script(opts)
 					vim.schedule(function()
 						vim.cmd("tabnew")
 
-						vim.cmd("Neotest summary")
+						local neotest = require("neotest")
 
+						-- Open the summary in this tab
+						neotest.summary.open()
+
+						-- Close all non-summary windows in this tab
 						local wins = vim.api.nvim_tabpage_list_wins(0)
 						for _, win in ipairs(wins) do
 							local buf = vim.api.nvim_win_get_buf(win)
@@ -76,7 +152,8 @@ local function pick_npm_script(opts)
 							end
 						end
 
-						vim.cmd("Neotest output-panel")
+						-- Open the output panel and move it to the left
+						neotest.output_panel.open()
 						vim.cmd("wincmd H")
 						vim.cmd("vertical resize 80")
 					end)
